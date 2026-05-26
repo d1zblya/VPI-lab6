9f53bd46358f5cb685186ecc1eb95250400ed43b
Artyom <artyom@MacBook-Pro-Artyom.local>
2026-05-26 09:25:14 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QtWidgets>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QMainWindow *mw = new QMainWindow(0, Qt::Window);
+    mw->setWindowTitle("Hello, Qt5");
+    mw->resize(400, 300);
+    mw->show();
+    return app.exec();
+}
