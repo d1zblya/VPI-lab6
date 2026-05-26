81ba82e1c32b5718ccdfcebccadeafde97f2531b
Artyom <delangame666@gmail.com>
2026-05-26 09:52:34 +0300

    feature: widget with square calculation

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
+#include "mywidget.h"
 #include <QtWidgets>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QWidget window;
-    window.resize(200, 120);
-    window.setWindowTitle("Button");
-    QPushButton quit("Quit", &window);
-    quit.setFont(QFont("Times", 18, QFont::Bold));
-    quit.setGeometry(10, 40, 180, 40);
-    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
-    window.show();
+    MyWidget widget;
+    widget.show();
     return app.exec();
 }
---
+++ mywidget.h
@@ -0,0 +1,22 @@
+#ifndef MYWIDGET_H
+#define MYWIDGET_H
+#include <QString>
+#include <QtWidgets>
+class MyWidget : public QMainWindow {
+    Q_OBJECT
+  private:
+    QLineEdit *line1;
+    QLineEdit *line2;
+    QPushButton *bsqua;
+    QPushButton *bclear;
+    QLabel *labsign;
+    QLabel *labis;
+  private slots:
+    void slotClear();
+    void slotSqua();
+
+  public:
+    MyWidget();
+    ~MyWidget();
+};
+#endif
